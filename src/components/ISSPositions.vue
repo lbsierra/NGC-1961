6649b7a4f61ab7304f78403409e8f51ebf244954
Sierra <lbsierra@outlook.com>
2025-08-07 19:04:30 -0400

    Expanded exoplanet catalog

--- src/components/ISSPositions.vue
+++ src/components/ISSPositions.vue
@@ -4,6 +4,9 @@
         <p v-if="position">
             Latitude: {{ position.latitude }}, Longitude: {{ position.longitude }}
         </p>
+        <p v-if="location">
+            Over: <strong>{{ location }}</strong>
+        </p>
         <p v-else>
             Loading ISS data...
         </p>         
@@ -12,12 +15,32 @@
 
 <script setup>
     import { ref, onMounted } from 'vue';
-    import '../env-test.js';
 
     const position = ref(null);
+    const location = ref('');
+
+      async function getLocation() {
+        const url = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lon}&zoom=5&addressdetails=1`;
+
+        try {
+            const response = await fetch(url);
+            const data = await response.json();
+            return data.address?.country || 'Unknown Location';
+        } catch (err) {
+            console.error('Reverse geocoding failed:', err);
+            return 'Unknown Location';
+        }
+    }
+
     onMounted(async () => {
         const res = await fetch('http://api.open-notify.org/iss-now.json');
         const data = await res.json();
         position.value = data.iss_position;
+        const lat = parseFloat(position.value.latitude);
+        const lon = parseFloat(position.value.longitude);
+
+        location.value = await getLocation(lat, lon);
     });
+
+  
 </script>
